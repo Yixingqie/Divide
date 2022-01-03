6074d5e872e70a66967f8226f57a3c53902a981f
Yixing Qie <yixingqie@gmail.com>
2022-01-02 21:32:14 -0500

    Added Tabs and History Page

---
+++ app/src/main/java/com/example/billsplit/FragmentAdapter.java
@@ -0,0 +1,31 @@
+package com.example.billsplit;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class FragmentAdapter extends FragmentStateAdapter {
+
+    public FragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new MembersFragment();
+            case 1:
+                return new ReceiptFragment();
+        }
+        return new TotalsFragment();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
--- app/src/main/java/com/example/billsplit/MainActivity.java
+++ app/src/main/java/com/example/billsplit/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.billsplit;
 
-import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -13,61 +10,91 @@ import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.Text;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.provider.MediaStore;
 import android.util.Log;
 import android.util.SparseArray;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.billsplit.ui.main.SectionsPagerAdapter;
-import com.example.billsplit.databinding.ActivityMainBinding;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageview;
-    Button btnUpload;
+    //Button btnUpload;
     TextView txtView;
     Bitmap bitmap;
-   ArrayList<String> priceList =new ArrayList<String>();
+    ArrayList<String> priceList =new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnUpload = findViewById(R.id.btnUpload);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        TabItem tabMembers = findViewById(R.id.members_tab);
+        TabItem tabReceipt = findViewById(R.id.receipt_tab);
+        TabItem tabTotals = findViewById(R.id.totals_tab);
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentAdapter pagerAdapter = new FragmentAdapter(fm, getLifecycle());
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setCurrentItem(0);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+      //  btnUpload = findViewById(R.id.btnUpload);
         txtView = findViewById(R.id.txtView);
         bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(),
                 R.drawable.ocr_sample);
 
-        btnUpload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CropImage.activity()
-                        .setGuidelines(CropImageView.Guidelines.ON)
-                        .start(MainActivity.this);
-            }
-        });
+//        btnUpload.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                CropImage.activity()
+//                        .setGuidelines(CropImageView.Guidelines.ON)
+//                        .start(MainActivity.this);
+//            }
+//        });
     }
 
     @Override
