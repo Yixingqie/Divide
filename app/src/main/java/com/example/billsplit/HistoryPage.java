23f5153b8e50c1299455ea03a07c176e90f8604d
Yixing Qie <yixingqie@gmail.com>
2022-01-05 21:10:50 -0500

    Added SplashScreen

--- app/src/main/java/com/example/billsplit/HistoryPage.java
+++ app/src/main/java/com/example/billsplit/HistoryPage.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
---
+++ app/src/main/java/com/example/billsplit/Splash.java
@@ -0,0 +1,36 @@
+package com.example.billsplit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    private static boolean splashLoaded = false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (!splashLoaded) {
+            setContentView(R.layout.activity_splash);
+            int secondsDelayed = 1;
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    startActivity(new Intent(Splash.this, HistoryPage.class));
+                    finish();
+                }
+            }, secondsDelayed * 1000);
+
+            splashLoaded = true;
+        }
+        else {
+            Intent goToMainActivity = new Intent(Splash.this, HistoryPage.class);
+            goToMainActivity.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(goToMainActivity);
+            finish();
+        }
+    }
+}
