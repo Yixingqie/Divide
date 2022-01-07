d48367f192a42c88bfa9677597781150ba0ed56d
Yixing Qie <yixingqie@gmail.com>
2022-01-06 22:11:11 -0500

    Add home/history button to totals page and fix add button on history

--- app/src/main/java/com/example/billsplit/TotalsFragment.java
+++ app/src/main/java/com/example/billsplit/TotalsFragment.java
@@ -1,5 +1,6 @@
 package com.example.billsplit;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +62,15 @@ public class TotalsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_totals, container, false);
+        View view = inflater.inflate(R.layout.fragment_totals, container, false);
+        FloatingActionButton homeFab = view.findViewById(R.id.homeBtn);
+        homeFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(),HistoryPage.class);
+                startActivity(i);
+            }
+        });
+        return view;
     }
 }
